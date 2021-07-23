87e842d91405156c663ee045a57e95f19dd3fcdb
MohamedAmineDHIAB <aminedhiab2028@gmail.com>
2021-07-23 23:55:42 +0200

    commit

---
+++ supervised/Classification/CNN-GNN/gnn.py
@@ -0,0 +1,38 @@
+import torch
+import torch.nn as nn
+
+import torchvision
+import torchvision.transforms as transforms
+import utils
+import numpy
+import cnn
+from matplotlib import pyplot as plt
+
+
+class GNN(torch.nn.Module):
+
+    def __init__(self, nbnodes, nbhid, nbclasses):
+        torch.nn.Module.__init__(self)
+        self.m = nbnodes
+        self.h = nbhid
+        self.c = nbclasses
+
+        self.U = torch.nn.Parameter(torch.FloatTensor(
+            numpy.random.normal(0, nbnodes**-.5, [nbnodes, nbhid])))
+        self.W = torch.nn.Parameter(torch.FloatTensor(
+            numpy.random.normal(0, nbhid**-.5, [nbhid, nbhid])))
+        self.V = torch.nn.Parameter(torch.zeros([nbhid, nbclasses]))
+
+    def forward(self, A):
+        # ------------------------------------
+        # TODO: Replace by your code
+        # ------------------------------------
+        import solution
+        Y = solution.forward(self, A)
+        # ------------------------------------
+
+        return Y
+
+
+if __name__ == '__main__':
+    Xr, Tr, Xt, Tt = cnn.get_data()
