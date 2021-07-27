7eab75366e83e24c005c61e556ddd267ba29a3ef
MohamedAmineDHIAB <aminedhiab2028@gmail.com>
2021-07-27 17:56:05 +0200

    commit

--- unsupervised/PCA/pca.py
+++ unsupervised/PCA/pca.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 import seaborn as sb
 import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+import dataframe_image as dfi
 
 
 def pca(X, n):
@@ -26,7 +28,9 @@ def pca(X, n):
     n_components = n
     eigenvector_subset = sorted_eigenvectors[:, 0:n_components]
     nPC = eigenvector_subset
+    # project the meaned data on these n principal components
     X_reduced = X_meaned@nPC
+
     return(X_reduced)
 
 
@@ -37,6 +41,8 @@ if __name__ == '__main__':
     data = pd.read_csv(url, names=[
                        'sepal length', 'sepal width', 'petal length', 'petal width', 'target'])
 
+    data_styled = data.head().style.background_gradient()
+    dfi.export(data_styled, './figs/original_iris.png')
     # prepare the data
     x = data.iloc[:, 0:4]
 
@@ -47,11 +53,12 @@ if __name__ == '__main__':
     x_reduced = pca(x, 2)
 
     # Creating a Pandas DataFrame of embedded Dataset
-    reduced_df = pd.DataFrame(
-        x_reduced, columns=['Principal Comp 1', 'Principal Comp 2'])
+    x_reduced.columns = ['Principal Comp 1', 'Principal Comp 2']
 
     # Concat it with target variable to create a complete Dataset
-    reduced_df = pd.concat([reduced_df, pd.DataFrame(target)], axis=1)
+    reduced_df = pd.concat([x_reduced, pd.DataFrame(target)], axis=1)
+
     plt.figure(figsize=(6, 6))
     sb.scatterplot(data=reduced_df, x='Principal Comp 1', y='Principal Comp 2',
-                   hue='target', s=60, palette='rocket')
+                   hue="target", s=60, palette='bright', style="target")
+    plt.savefig('./figs/pca_iris.png')
