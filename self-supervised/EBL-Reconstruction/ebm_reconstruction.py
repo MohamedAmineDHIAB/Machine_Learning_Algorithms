3e12c142c48717291177613e3d86c65861fa81dc
MohamedAmineDHIAB <aminedhiab2028@gmail.com>
2021-08-01 23:36:30 +0200

    commit

--- self-supervised/EBL-Reconstruction/ebm_reconstruction.py
+++ self-supervised/EBL-Reconstruction/ebm_reconstruction.py
@@ -4,6 +4,8 @@ import utils
 import numpy
 from matplotlib import pyplot as plt
 from sklearn.decomposition import PCA
+import torch.optim as optim
+from tqdm import tqdm as tq
 
 
 def removepatch(X):
@@ -16,20 +18,56 @@ def removepatch(X):
     return (X*(1-mask)).data, mask
 
 
+def train(enet, gnet, epochs=100, lrate=0.05, mini_batch=100):
+
+    N = 10000
+
+    optimizer = optim.SGD(list(enet.parameters()) +
+                          list(gnet.parameters()), lr=lrate)
+
+    for epoch in tq(epochs):
+
+        for i in range(N//mini_batch):
+
+            optimizer.zero_grad()
+
+            # Take a minibatch and train it
+            x = Xr[mb*i:mb*(i+1)].data*1.0
+            z, m = removepatch(x)
+
+            # Build the forward pass from the input until the loss function
+
+            xgen = gnet(x)
+            edata = enet(x)
+            egen = enet(xgen)
+
+            err = torch.log(1+torch.exp(edata-egen)).mean()
+
+            # Compute the gradient and perform one step of gradient descent
+            err.backward()
+            optimizer.step()
+
+        if epoch % 10 == 0:
+            print(epoch, err)
+
+
 if __name__ == '__main__':
     Xr, Xt = utils.getdata()
     xmask = removepatch(Xt[:10])[0]
     torch.manual_seed(0)
+    # build energy model
     enet = nn.Sequential(
         nn.Linear(784, 256), nn.Hardtanh(),
         nn.Linear(256, 256), nn.Hardtanh(),
         nn.Linear(256, 1),
     )
+    # build generative model
+    gnet = nn.Sequential(
+        nn.Linear(784, 256), nn.Hardtanh(),
+        nn.Linear(256, 256), nn.Hardtanh(),
+        nn.Linear(256, 784), nn.Hardtanh()
+    )
 
     Xn, m = removepatch(Xt[:10])
-
-    utils.vis10(pca(Xn, Xr, 10)*m+Xn*(1-m))
-    utils.vis10(pca(Xn, Xr, 60)*m+Xn*(1-m))
-    utils.vis10(pca(Xn, Xr, 360)*m+Xn*(1-m))
 
     plt.show()
